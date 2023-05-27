62a79d6e7d023db15b5c24450a452c3febf80f15
Rune Soerensen <runesoerensen@gmail.com>
2023-05-26 20:45:10 -0400

    Configure default authentication and challenge scheme to jwt auth

--- WirescaleApi/Program.cs
+++ WirescaleApi/Program.cs
@@ -1,6 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+});
 
 var app = builder.Build();
 
