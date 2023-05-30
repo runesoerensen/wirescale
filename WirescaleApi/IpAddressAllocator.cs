1e97bc81b1b0bf5c94f59b971ef9d597345bd443
Rune Soerensen <runesoerensen@gmail.com>
2023-05-30 00:04:18 -0400

    Add IpAddressAllocator#AllocateIpAddress

--- WirescaleApi/IpAddressAllocator.cs
+++ WirescaleApi/IpAddressAllocator.cs
@@ -1,5 +1,12 @@
-﻿namespace WirescaleApi;
+﻿using System.Net;
+using NetTools;
+
+namespace WirescaleApi;
 
 public class IpAddressAllocator
 {
+    public virtual IPAddress AllocateIpAddress(string networkCidr, IEnumerable<IPAddressRange> peerIpRanges)
+    {
+        throw new NotImplementedException();
+    }
 }
