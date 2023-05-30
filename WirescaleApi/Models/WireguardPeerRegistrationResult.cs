9da30ef4af2079f0b7a8ccf118a270583ff593f0
Rune Soerensen <runesoerensen@gmail.com>
2023-05-30 01:02:54 -0400

    Add relevant properties to peer registration model

--- WirescaleApi/Models/WireguardPeerRegistrationResult.cs
+++ WirescaleApi/Models/WireguardPeerRegistrationResult.cs
@@ -2,7 +2,11 @@
 
 public class WireguardPeerRegistrationResult
 {
-	public WireguardPeerRegistrationResult()
-	{
-	}
+    public string PublicKey { get; set; }
+    public string UrlSafePublicKey { get; set; }
+    public List<string> AllowedIps { get; set; }
+
+    public string ServerPublicKey { get; set; }
+    public string ServerEndpoint { get; set; }
+    public List<string> ServerAllowedIps { get; set; }
 }
