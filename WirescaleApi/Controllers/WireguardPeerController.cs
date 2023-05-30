1acb140143c4ab6f06640706b0d39c261f21a792
Rune Soerensen <runesoerensen@gmail.com>
2023-05-30 00:44:27 -0400

    Rename WireguardPeer to WireguardPeerRegistrationResult

--- WirescaleApi/Controllers/WireguardPeerController.cs
+++ WirescaleApi/Controllers/WireguardPeerController.cs
@@ -10,7 +10,7 @@ public class WireguardPeerController : ControllerBase
 {
     [HttpPost]
     [Authorize]
-    public async Task<WireguardPeer> Create([FromBody] string publicKey)
+    public async Task<WireguardPeerRegistrationResult> Create([FromBody] string publicKey)
     {
         throw new NotImplementedException();
     }
