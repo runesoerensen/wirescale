a3c9e4982cdb486535c64825a572e472b712a755
Rune Soerensen <runesoerensen@gmail.com>
2023-05-29 23:48:10 -0400

    Register and configure HttpClient for WgRestApiClient

--- WirescaleApi/Program.cs
+++ WirescaleApi/Program.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
+using System.Net.Http.Headers;
+using WirescaleApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,12 @@ builder.Services.AddAuthentication(options =>
 {
     options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
     options.Audience = builder.Configuration["Auth0:Audience"];
+});
+
+builder.Services.AddHttpClient<IWgrestApiClient, WgrestApiClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["WgrestApi:BaseAddress"]);
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", builder.Configuration["WgrestApi:AuthToken"]);
 });
 
 var app = builder.Build();
