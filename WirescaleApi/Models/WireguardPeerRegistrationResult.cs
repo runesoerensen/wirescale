1f9cf4098cd7426966d1d8ff270f913da6d01353
Rune Soerensen <runesoerensen@gmail.com>
2023-05-30 01:07:16 -0400

    Add comments to registration result properties
    Ideally this can be handled with better naming. Suggestions more than welcome

--- WirescaleApi/Models/WireguardPeerRegistrationResult.cs
+++ WirescaleApi/Models/WireguardPeerRegistrationResult.cs
@@ -2,11 +2,11 @@
 
 public class WireguardPeerRegistrationResult
 {
-    public string PublicKey { get; set; }
-    public string UrlSafePublicKey { get; set; }
-    public List<string> AllowedIps { get; set; }
+    public string PublicKey { get; set; }               // This is the same public key the user provided for registration.
+    public string UrlSafePublicKey { get; set; }        // Client could use this with other RESTful endpoints, e.g. to GET/DELETE the peer registration.
+    public List<string> AllowedIps { get; set; }        // Client will use this to set their assigned virtual network address(es).
 
-    public string ServerPublicKey { get; set; }
-    public string ServerEndpoint { get; set; }
-    public List<string> ServerAllowedIps { get; set; }
+    public string ServerPublicKey { get; set; }         // Client will use this to register the server peer locally.
+    public string ServerEndpoint { get; set; }          // The server's public IP address and port.
+    public List<string> ServerAllowedIps { get; set; }  // IP ranges that the server can route traffic to on the virtual network.
 }
