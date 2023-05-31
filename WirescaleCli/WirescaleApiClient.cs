24fc16b09e716b061e209daa45f80f1489974a0a
Rune Soerensen <runesoerensen@gmail.com>
2023-05-31 16:40:25 -0400

    Add WirescaleApiClient

---
+++ WirescaleCli/WirescaleApiClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+
+namespace WirescaleCli;
+
+public class WirescaleApiClient
+{
+    private readonly string _baseUri;
+
+    public WirescaleApiClient(string baseUri)
+    {
+        _baseUri = baseUri;
+    }
+
+    public async Task<WirescalePeerRegistration> RegisterPublicKey(string accessToken, WireguardKeyPair wireguardKeyPair)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+            var response = await httpClient.PostAsJsonAsync($"{_baseUri}/wireguardpeer", wireguardKeyPair.PublicKey);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<WirescalePeerRegistration>();
+        }
+    }
+}
