c922a87cb8efe4eae639ad3ff3447de9435f314b
Rune Soerensen <runesoerensen@gmail.com>
2023-05-31 16:36:09 -0400

    Add WireguardKeyPairGenerator

---
+++ WirescaleCli/WireguardKeyPairGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+namespace WirescaleCli;
+
+public class WireguardKeyPairGenerator
+{
+    public WireguardKeyPair Generate()
+    {
+        // Generate a new private key
+        var privateKeyProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "wg",
+                Arguments = "genkey",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        privateKeyProcess.Start();
+        var privateKey = privateKeyProcess.StandardOutput.ReadToEnd().Trim();
+        privateKeyProcess.WaitForExit();
+
+        // Generate the corresponding public key
+        var publicKeyProcess = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "wg",
+                Arguments = "pubkey",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        publicKeyProcess.Start();
+        publicKeyProcess.StandardInput.WriteLine(privateKey);
+        publicKeyProcess.StandardInput.Flush();
+        publicKeyProcess.StandardInput.Close();
+
+        var publicKey = publicKeyProcess.StandardOutput.ReadToEnd().Trim();
+
+        publicKeyProcess.WaitForExit();
+
+        return new WireguardKeyPair
+        {
+            PublicKey = publicKey,
+            PrivateKey = privateKey,
+        };
+    }
+}
