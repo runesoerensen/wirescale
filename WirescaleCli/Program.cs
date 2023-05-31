feb401570aa5fa5e56ca336bb77df5160f717c46
Rune Soerensen <runesoerensen@gmail.com>
2023-05-31 16:28:01 -0400

    Initialize appsettings.json configuration

--- WirescaleCli/Program.cs
+++ WirescaleCli/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace WirescaleCli;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WirescaleCli;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+
+        var configuration = configurationBuilder.Build();
     }
 }
-
